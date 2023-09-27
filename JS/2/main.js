0fe8e13a12aab1e6e4f0cd622ff5a6e1ac90a6bf
Illia Teliuk <illiateliuk@gmail.com>
2023-09-27 22:58:14 +0300

    added method to push values to the end of an array

--- JS/2/main.js
+++ JS/2/main.js
@@ -1,2 +1,12 @@
 const firstVar = 250;
 console.log(firstVar);
+
+let myFriends = [];
+
+function AddMyFriends(friend) {
+    myFriends.push(friend);
+    console.log(myFriends);
+}
+
+AddMyFriends("illia");
+AddMyFriends("petro");
