0e301e27327b55a7457cdd33fea6435ba4ae781d
Illia Teliuk <illiateliuk@gmail.com>
2023-09-29 00:31:29 +0300

    Finished javascript section. Need to study some topics deeper before going to react

--- JS/2/main.js
+++ JS/2/main.js
@@ -1,72 +1,100 @@
-// // // const firstVar = 250;
-// // // console.log(firstVar);
+// // // // // // const firstVar = 250;
+// // // // // // console.log(firstVar);
 
-// // // let myFriends = [];
+// // // // // // let myFriends = [];
 
-// // // function AddMyFriends(friend) {
-// // //     myFriends.push(friend);
-// // //     console.log(myFriends);
-// // // }
+// // // // // // function AddMyFriends(friend) {
+// // // // // //     myFriends.push(friend);
+// // // // // //     console.log(myFriends);
+// // // // // // }
 
-// // // AddMyFriends("illia");
-// // // AddMyFriends("petro");
+// // // // // // AddMyFriends("illia");
+// // // // // // AddMyFriends("petro");
 
-// // const age = 19;
+// // // // // const age = 19;
 
-// // function AgeAge() {
-// //     const age = 23;
-// //     console.log(age);
+// // // // // function AgeAge() {
+// // // // //     const age = 23;
+// // // // //     console.log(age);
+// // // // // }
+
+// // // // // AgeAge();
+// // // // // console.log(age);
+
+// // // // let heading = document.querySelector('.helloWorld');
+
+// // // // let listItems = document.querySelectorAll('ul li');
+
+// // // // for(let i = 0; i<listItems.length; i++){
+// // // //     listItems[i].innerText = "Bruh";
+// // // // }
+
+// // // // heading.innerText = "Hello Illia!!";
+
+// // const incrementBtn = document.querySelector('#increment');
+// // const decrementBtn = document.querySelector('#decrement');
+// // const counterEl = document.getElementById('counter');
+// // const ulElement = document.getElementById('list-items');
+// // let counter = 0;
+
+// // function incrementCounter() {
+// //     counter++;
+// //     counterEl.innerText = counter;
+
+// //     //Create the element
+// //     const li = document.createElement('li');
+// //     li.setAttribute('data-counter', counter);
+// //     li.innerHTML = '<b>Something</b> ' + counter;
+// //     if(counter % 2 == 0){
+// //         li.classList.add('class', 'yellow');
+// //     }
+// //     else{
+// //         li.setAttribute('class', 'blue');
+// //     }
+// //     // const b = document.createElement('b');
+// //     // // li.innerText = ("Item " + count);
+// //     // const textNode = document.createTextNode("Item ");
+// //     // b.appendChild(textNode);
+// //     // const textNode2 = document.createTextNode(count);
+// //     // li.appendChild(b);
+// //     // li.appendChild(textNode2);
+
+
+
+// //     //Append the elemnt to the list
+// //     ulElement.appendChild(li);
 // // }
 
-// // AgeAge();
-// // console.log(age);
+// // // function decrementCounter() {
+// // //     const li = ulElement.querySelector('[data-counter = "' + counter +'"]');
+// // //     li.remove();
+// // //     counter--;
+// // //     counterEl.innerText = counter;
 
-// let heading = document.querySelector('.helloWorld');
+// // // }
 
-// let listItems = document.querySelectorAll('ul li');
+// // incrementBtn.addEventListener('click', incrementCounter);
+// // // decrementBtn.addEventListener('click', decrementCounter);
 
-// for(let i = 0; i<listItems.length; i++){
-//     listItems[i].innerText = "Bruh";
-// }
+// // const arr1 = [1, 2, 3];
+// // const arr2 = [4, 5, 6];
+// // const arr = [...arr1, arr2]
+// // console.log(arr)
 
-// heading.innerText = "Hello Illia!!";
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-const incrementBtn = document.querySelector('#increment');
-const decrementBtn = document.querySelector('#decrement');
-const counterEl = document.getElementById('counter');
-const ulElement = document.getElementById('list-items');
-let counter = 0;
+// console.log(arr);
 
-function incrementCounter() {
-    counter++;
-    counterEl.innerText = counter;
-
-    //Create the element
-    const li = document.createElement('li');
-    li.setAttribute('data-counter', counter);
-    li.innerHTML = '<b>Something</b> ' + counter;
-    console.log(li)
-    // const b = document.createElement('b');
-    // // li.innerText = ("Item " + count);
-    // const textNode = document.createTextNode("Item ");
-    // b.appendChild(textNode);
-    // const textNode2 = document.createTextNode(count);
-    // li.appendChild(b);
-    // li.appendChild(textNode2);
+// arr.forEach(function(element) {element = element**2});
 
 
 
-    //Append the elemnt to the list
-    ulElement.appendChild(li);
-}
+// console.log(arr);
 
-function decrementCounter() {
-    const li = ulElement.querySelector('[data-counter = "' + counter +'"]');
-    li.remove();
-    counter--;
-    counterEl.innerText = counter;
-    
-}
-
-incrementBtn.addEventListener('click', incrementCounter);
-decrementBtn.addEventListener('click', decrementCounter);
+const promiseObject = fetch('data.json');
+promiseObject.then(response => {
+    const promiseObject2 = response.json();
+    promiseObject2.then(data => {
+        console.log(data);
+    })
+});
