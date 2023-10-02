284c989c6a72663f0c8859e00f9eb58a1a99bece
Illia Teliuk <illiateliuk@gmail.com>
2023-10-03 01:28:43 +0300

    Created the first react app in tutorial

--- JS/Events/main.js
+++ JS/Events/main.js
@@ -23,3 +23,45 @@ function menu(event) {
 		menuBody.classList.remove('_active');
 	}
 }
+
+// let mouseBlock = document.querySelector('.block-for-mouse');
+
+
+// mouseBlock.addEventListener("mouseenter", (event) => {
+// 	mouseBlock.innerHTML = 
+// 	`<span class = "block-mouse-text"> ClientX: ${event.clientX} <br> ClientY: ${event.clientY} </span>`
+// })
+// mouseBlock.addEventListener("mouseout", () => {
+// 	mouseBlock.innerHTML = 
+// 	""
+// })
+
+// let mouseBlock = document.querySelector('.block-for-mouse');
+// let mouseBlocktext = document.querySelector('.block-mouse-text');
+
+
+// mouseBlock.addEventListener("mousemove", (event) => {
+// 	mouseBlocktext.innerHTML =
+// 	`ClientX: ${event.clientX} <br> ClientY: ${event.clientY} `
+		
+// })
+
+
+// mouseBlock.addEventListener("mouseout", () => {
+// 	mouseBlocktext.innerHTML =
+// 		""
+// })
+
+const blockForMouse = document.querySelector('.block-for-mouse');
+blockForMouse.addEventListener("mouseover", function (event) {
+	let target = event.target.closest('span');
+	// переход не на <span> - игнорировать
+	if (!target) return;
+	target.style.cssText = `background-color: #${Math.floor(Math.random()*16777215).toString(16)}`;
+});
+blockForMouse.addEventListener("mouseout", function (event) {
+	let target = event.target.closest('span');
+	// переход не на <span> - игнорировать
+	if (!target) return;
+	target.style.cssText = ``;
+});
