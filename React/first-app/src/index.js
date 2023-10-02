fea294f30cbadab06b4a5060ac462516a81ec051
Illia Teliuk <illiateliuk@gmail.com>
2023-10-03 02:30:46 +0300

    Keep going with the react tutorial

--- React/first-app/src/index.js
+++ React/first-app/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { add } from './calculator';
+
+console.log(add(4, 5))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+root.render(<h1>Hello world!</h1>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
