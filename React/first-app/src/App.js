d41ab7c923efe60eb0fc8e9a710241dbfc842ad4
Illia Teliuk <illiateliuk@gmail.com>
2023-10-04 23:34:38 +0300

    Studied useState, some event listeners

--- React/first-app/src/App.js
+++ React/first-app/src/App.js
@@ -1,9 +1,33 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Custom from './components/Custom';
 
+
+
+
+
+
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const btnStyle = {
+    margin:  '0 0 0 10px'
+  }
+
+  function buttonClickFunction(){
+    console.log("I was clicked");
+  }
+
+  function increaseCounter(){
+    setCounter(counter + 1)
+  }
+
+  function decreaseCounter(){
+    setCounter(counter - 1)
+  }
+
   return (
     <div className="App">
         <Header />
@@ -11,7 +35,10 @@ function App() {
           This is content
         </div>
         <h1>Hello, world {2+2} </h1>
-        <Custom name = "Illia" />
+        <button onClick={increaseCounter}>Increase the counter</button>
+        <button onClick={decreaseCounter} style={btnStyle}>Decrease the counter</button>
+        <Custom name = "Illia" greeting = "Привіт"/>
+        <h1>Counter: {counter}</h1>
     </div>
   );
 }
