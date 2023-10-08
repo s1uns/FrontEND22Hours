7fe5df7e16af4f6fa9e231dd43c7fae9aaa4c78f
Illia Teliuk <illiateliuk@gmail.com>
2023-10-09 00:02:02 +0300

    Changed the default button with my own component and styled it

--- React/todo-list/src/components/layouts/Modal.jsx
+++ React/todo-list/src/components/layouts/Modal.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import ReactDom  from 'react-dom';
+import ReactDom from 'react-dom';
+import Button from './Button';
 import './styles/Modal.css'
 
-export default function Modal({open, children, onClose}) {
-    if(!open) return null;
+export default function Modal({ open, children, onClose }) {
+  if (!open) return null;
   return ReactDom.createPortal(
     <div className="overlay">
-        <div className='modal'>
-        <button onClick={onClose}>Close</button>
+      <div className='modal'>
         {children}
-    </div>
+        <Button className='close-modal-btn' onClick={onClose}>Close</Button>
+      </div>
     </div>
     ,
     document.getElementById('portal')
