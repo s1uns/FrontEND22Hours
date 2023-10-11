4fe78e3bad803c6b32f95e860a7cf57b25c2f1e1
Illia Teliuk <illiateliuk@gmail.com>
2023-10-12 02:01:36 +0300

    tried to addimg to the task

--- React/todo-list/src/components/ToDo/Task.jsx
+++ React/todo-list/src/components/ToDo/Task.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import './styles/Task.css'
+// import exampleImg from '../../img/example.jpg'
 
 function Task(props) {
     const { id, name, description, image, ...other } = props
 
     return (
         <div className="task">
+            {/* <div className="img-container">
+                <img className="img" src={exampleImg} alt="" />
+            </div> */}
             <div className="task-info">
                 <h6>{name}</h6>
                 <p>{description.length < 30 ? description : `${description.slice(0, 27)}...`}</p>
