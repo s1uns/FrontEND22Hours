1770c8a1c975532c9af12781c22a874f7c8fa1ce
Illia Teliuk <illiateliuk@gmail.com>
2023-10-16 22:50:09 +0300

    Added text box component to be placed between the parallax items. Fixed the issue with white spaces

--- React/parallax-react/src/App.js
+++ React/parallax-react/src/App.js
@@ -1,12 +1,15 @@
 import ParallaxOne from "./components/ParallaxOne";
 import ParallaxTwo from "./components/ParallaxTwo";
 import ParallaxThree from "./components/ParallaxThree";
+import TextBox from "./components/TextBox";
 
 function App() {
     return (
         <div>
             <ParallaxOne />
+            <TextBox />
             <ParallaxTwo />
+            <TextBox />
             <ParallaxThree />
         </div>
     );
