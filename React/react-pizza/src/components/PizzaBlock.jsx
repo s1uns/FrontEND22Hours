a7bfaff14a9d95c1f5443a4b820e01ba9dfdbaa7
Illia Teliuk <illiateliuk@gmail.com>
2023-10-17 01:24:05 +0300

    Created the logic fof the 'Add' button, changed classes to classNames

--- React/react-pizza/src/components/PizzaBlock.jsx
+++ React/react-pizza/src/components/PizzaBlock.jsx
@@ -1,44 +1,49 @@
-import React from 'react'
+import React, { useState } from "react";
 
-export default function PizzaBlock(props) {
-  return (
-    <div class="pizza-block">
-    <img
-        class="pizza-block__image"
-        src="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
-        alt="Pizza"
-    />
-    <h4 class="pizza-block__title">{props.title}</h4>
-    <div class="pizza-block__selector">
-        <ul>
-            <li>thin</li>
-            <li>traditional</li>
-        </ul>
-        <ul>
-            <li>26 cm.</li>
-            <li>30 cm.</li>
-            <li>40 cm.</li>
-        </ul>
-    </div>
-    <div class="pizza-block__bottom">
-        <div class="pizza-block__price">from {props.price} $</div>
-        <div class="button button--outline button--add">
-            <svg
-                width="12"
-                height="12"
-                viewBox="0 0 12 12"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-            >
-                <path
-                    d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
-                    fill="white"
-                />
-            </svg>
-            <span>Add</span>
-            <i>2</i>
+export default function PizzaBlock({ title, price }) {
+    const [pizzaCount, setPizzaCount] = useState(0);
+    const onClickAdd = () => {
+        setPizzaCount(pizzaCount + 1);
+    }
+
+    return (
+        <div className="pizza-block">
+            <img
+                className="pizza-block__image"
+                src="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
+                alt="Pizza"
+            />
+            <h4 className="pizza-block__title">{title}</h4>
+            <div className="pizza-block__selector">
+                <ul>
+                    <li className="active">thin</li>
+                    <li>traditional</li>
+                </ul>
+                <ul>
+                    <li className="active">26 cm.</li>
+                    <li>30 cm.</li>
+                    <li>40 cm.</li>
+                </ul>
+            </div>
+            <div className="pizza-block__bottom">
+                <div className="pizza-block__price">from {price} $</div>
+                <button className="button button--outline button--add"  onClick={onClickAdd}>
+                    <svg
+                        width="12"
+                        height="12"
+                        viewBox="0 0 12 12"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
+                            fill="white"
+                        />
+                    </svg>
+                    <span>Add</span>
+                    <i>{pizzaCount}</i>
+                </button>
+            </div>
         </div>
-    </div>
-</div>
-  )
+    );
 }
