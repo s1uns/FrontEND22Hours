3b8febdc5b990260e656b9419b5b047e463461b9
Illia Teliuk <illiateliuk@gmail.com>
2023-10-18 00:44:31 +0300

    Moved all the categories to an array and made it render through mapping

--- React/react-pizza/src/components/Categories.jsx
+++ React/react-pizza/src/components/Categories.jsx
@@ -1,21 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 export default function Categories() {
     const [activeIndex, setActiveIndex] = useState(5);
-
-    const changeIndex = (index) => {
-        setActiveIndex(index);
-    };
+    const categories = [
+        "All",
+        "Meat",
+        "Vegetarian",
+        "Grill",
+        "Spicy",
+        "Closed",
+    ];
 
     return (
         <div className="categories">
             <ul>
-                <li onClick={() => changeIndex(0)} className= {activeIndex === 0 ? 'active' : ''}>All</li>
-                <li onClick={() => changeIndex(1)} className= {activeIndex === 1 ? 'active' : ''}>Meat</li>
-                <li onClick={() => changeIndex(2)} className= {activeIndex === 2 ? 'active' : ''}>Vegetarian</li>
-                <li onClick={() => changeIndex(3)} className= {activeIndex === 3 ? 'active' : ''}>Grill</li>
-                <li onClick={() => changeIndex(4)} className= {activeIndex === 4 ? 'active' : ''}>Spicy</li>
-                <li onClick={() => changeIndex(5)} className= {activeIndex === 5 ? 'active' : ''}>Closed</li>
+                {categories.map((item, index) => {
+                    return (
+                        <li
+                            key={index}
+                            onClick={() => setActiveIndex(index)}
+                            className={activeIndex === index ? "active" : ""}
+                        >
+                            {item}
+                        </li>
+                    );
+                })}
             </ul>
         </div>
     );
